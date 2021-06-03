b2372f9c1c80abeea3e486e1dabc0f190164747b
abhishek <abhishek.dhule.79@gmail.com>
2021-06-03 20:09:48 +0530

    created form for questions

--- assessment/views.py
+++ assessment/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Assessment,Question,AssessmentScore
+from . forms import QuestionForm
 # Create your views here.
 
 def recruitersPage(request):
@@ -8,6 +9,10 @@ def recruitersPage(request):
     return render(request,'assessment/recruiter.html',context={"assessments":assessments})
 
 def questionsPage(request,assgK):
+    form=QuestionForm()
     assessment=Assessment.objects.get(id=assgK)
     questions=Question.objects.filter(assessment=assessment)
-    return render(request,'assessment/que.html',{"questions":questions})
+    return render(request,'assessment/que.html',{"questions":questions,"form":form})
+
+def createAssessment(request,assgK):
+    pass
