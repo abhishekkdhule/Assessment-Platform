4689045912f3cf08bfc806b8a4122b46802cf8f1
abhishek <abhishek.dhule.79@gmail.com>
2021-06-03 18:53:36 +0530

    assessment app created

---
+++ assessment/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from user.models import User
+
+
+class Assessment(models.Model):
+    title=models.CharField(max_length=200)
+    createdBy=models.ForeignKey(User,on_delete=models.CASCADE)
+    createdOn=models.DateTimeField(auto_now_add=True)
+    max_score=models.DecimalField(max_digits=6,decimal_places=2,default=0.0)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    answer_choice = [
+        ('a', 'option a'),
+        ('b', 'option b'),
+        ('c', 'option c'),
+        ('d', 'option d'),
+    ]
+    assessment=models.ForeignKey(Assessment,on_delete=models.CASCADE)
+    que=models.CharField(max_length=500)
+    op_a=models.CharField(max_length=300)
+    op_b=models.CharField(max_length=300)
+    op_c=models.CharField(max_length=300)
+    op_d=models.CharField(max_length=300)
+    marks_assigned=models.DecimalField(max_digits=4,decimal_places=2)
+    correct_choice=models.CharField(max_length=1,choices=answer_choice,default='a')
+
+    def __str__(self):
+        return "A"+str(self.assessment.id)
+
+
+class AssessmentScores(models.Model):
+    assessment=models.ForeignKey(Assessment,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    is_pending=models.BooleanField(default=False)
+    score_obtained=models.DecimalField(max_digits=6,decimal_places=2,default=0.0)
+
+    def __str__(self):
+        return "A"+str(self.assessment.id)+" U"+str(self.user.id)
+    
+    
