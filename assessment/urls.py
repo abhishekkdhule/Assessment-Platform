81fdd7ecf6db5e32e6a1cb29ffd91bbc7de916ca
abhishek <abhishek.dhule.79@gmail.com>
2021-06-04 19:11:24 +0530

    que form added

--- assessment/urls.py
+++ assessment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recruitersPage,questionsPage
+from .views import recruitersPage,questionsPage,createAssessment
 
 urlpatterns = [
     path('',recruitersPage,name='recruiter'),
+    path('newassm/<int:assgK>',createAssessment,name='newassm'),
     path('quetions/<int:assgK>',questionsPage,name='questions'),
     
 ]
--- assessment/views.py
+++ assessment/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Assessment,Question,AssessmentScore
 from . forms import QuestionForm
 # Create your views here.
 
 def recruitersPage(request):
-    
+    if request.method=="POST":
+        print(request.POST)
+        assessmentTitle=request.POST['assessment']
+        assmObj=Assessment.objects.create(title=assessmentTitle,createdBy=request.user)
+        print("assessment obj",assmObj)
+        return redirect('newassm',assmObj.id)
     assessments=Assessment.objects.filter(createdBy=request.user)
     return render(request,'assessment/recruiter.html',context={"assessments":assessments})
 
-def questionsPage(request,assgK):
+def createAssessment(request,assgK):
+    assmObj=Assessment.objects.get(id=assgK)
     form=QuestionForm()
+    context={
+        "assessment":assmObj,
+        "form":form
+    }
+    return render(request,'assessment/newassm.html',context)
+
+def questionsPage(request,assgK):
     assessment=Assessment.objects.get(id=assgK)
     questions=Question.objects.filter(assessment=assessment)
-    return render(request,'assessment/que.html',{"questions":questions,"form":form})
+    return render(request,'assessment/que.html',{"questions":questions})
 
-def createAssessment(request,assgK):
-    pass
